58bedac2938889325186cc2b6bfdd77a513165aa
saltonly <anyaluk4sova@yandex.ru>
2021-12-26 19:13:03 +0700

    project

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace gitt
     {
         static void Main(string[] args)
         {
-         
+            int n1 = 0; int n2 = 0;
+
+            Console.WriteLine("  ^ Казалось бы калькулятор ^  \r");
+            Console.WriteLine("_______________________________\n");
+
+
+
+
+
+
+
+            // Закрытие проги
+            Console.Write("Нажмите на любую кнопочку для закрытия ^ Казалось бы калькулятора ^) ");
+            Console.ReadKey();
         }
     }
 }
